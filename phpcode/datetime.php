fbec6b2a169e6e0400d6df4b0184351a0937a67c
NoahJRedell <noahredell@gmail.com>
2016-10-25 09:02:34 -0700

    Add files via upload

---
+++ phpcode/datetime.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+ini_set('display_errors','off');
+echo "<pre>";
+$str1 = $_SERVER['REMOTE_ADDR'];
+echo "$str1 <hr />";
+echo date('Y-m-d H:i:s');
+echo "</pre>";
+?>
+
+</body>
+</html>
---
+++ phpcode/hello2.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<?php
+	$color = "#FF0000";
+?>
+<body bgcolor=<?php  echo "$color"; ?>;
+
+<?php
+$txt1 = "India";
+$txt2 = "10.183.1.9";
+$txt3 = "ballclack ballclack on yo face";
+$x = 5;
+$y = 4;
+$z = 65521;
+
+echo "<h2>" . $txt1 . "</h2>";
+echo "My ip: " . $txt2 . "<br>";
+echo "$txt3";
+echo $x + $y + $z;
+?>
+
+</body>
+</html>
